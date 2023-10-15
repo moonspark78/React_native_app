a827aa998a857629db3efa0f2757d77329431c8e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-16 01:24:54 +0200

    :rocket:

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacit, Image, FlatList } from 'react-native'
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+          <Text style={styles.userName }>Hello Souli</Text>
+          <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+          <View style={styles.searchWrapper}>
+
+          </View>
+      </View>
     </View>
   )
 }
