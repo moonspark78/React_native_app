96e93c1ab2579772d854c79fa83cb45140c4a663
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-16 02:37:02 +0200

    :rocket:

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -17,9 +17,9 @@ const Welcome = () => {
           <View style={styles.searchWrapper}>
               <TextInput
                 style={styles.searchInput}
-                value=''
+                value=""
                 onChange={() => {}}
-                placeholder='What are you looking for?'
+                placeholder=""
               />
           </View>
       </View>
