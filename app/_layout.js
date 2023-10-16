0d424032fa0e83c3d7c3382b4303de19dd203e1a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-16 02:23:05 +0200

    :rocket:

--- app/_layout.js
+++ app/_layout.js
@@ -7,7 +7,25 @@ SplashScreen.preventAutoHideAsync();
 
 
 const Layout = () => {
-    return <Stack />;
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async ()  => {
+        if(fontsLoaded){
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+
+
+
+
+    return <Stack onLayout={onLayoutRootView}/>;
 }
 
 
